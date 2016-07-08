f1338fe8ac6964d28d8aee0f4a58aba6c2b001ed
Kristina Tietjen <KristinaTietjen@w134-87-131-075.wireless.uvic.ca>
2016-07-08 12:49:02 -0700

    made and edited csv file and worked on R

--- analyses/explore_survey_data.R
+++ analyses/explore_survey_data.R
@@ -9,17 +9,23 @@ setwd("/Users/kristinatietjen/Documents/Git_Hub/gya-research")
 
 
 ## read data
-survey<-read.csv("data/July-7-2016-7pm-Toronto.csv", header=FALSE)
+survey<-read.csv("data/July-7-2016-7pm-Toronto_simplified.csv", header=TRUE)
 colnames(survey)<-as.character(survey[1,])
-
+dim(survey)
+names(survey)
+head(survey)
 ## remove some unnecessary columns
-survey$Username<-NULL
-survey$'Updated At'<-NULL
-survey'Number of Saves'<-NULL
+#survey$Username<-NULL
+#survey$'Updated At'<-NULL
+#survey'Number of Saves'<-NULL
 
 
 ### keep complete data only
-survey<-survey[survey$Status="Complete",]
+survey<-survey[survey$Status=="Complete", ]
+dim(survey)
+
+
+
 
 # add 'date column'
 survey$date<-str_split_fixed(survey$'Completed At', " ", 1)
