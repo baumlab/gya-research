88973289340f9844bf0415cbe4dac2bc17069deb
James Robinson <jpwrobinson@gmail.com>
2016-07-08 11:10:01 -0700

    first cleaning

--- analyses/explore_survey_data.R
+++ analyses/explore_survey_data.R
@@ -9,7 +9,23 @@ setwd("/Users/kristinatietjen/Documents/gya-research")
 
 
 ## read data
-survey<-read.csv("data/July-7-2016-7pm-Toronto.csv")
+survey<-read.csv("data/July-7-2016-7pm-Toronto.csv", header=FALSE)
+survey<-survey[-1,]
+
+## remove some unnecessary columns
+survey$Username<-NULL
+survey$'Updated At'<-NULL
+survey'Number of Saves'<-NULL
 
 
+### keep complete data only
+survey<-survey[survey$Status="Complete",]
 
+# add 'date column'
+survey$date<-str_split_fixed(survey$'Completed At', " ", 1)
+
+################################
+#### Summary statistics ########
+################################
+
+aggregate(Status ~ Location, survey, function(x)length(unique(x)))
