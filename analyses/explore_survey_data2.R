9973e16731ee41b173d2751800588413e7c76dd3
Kristina Tietjen <KristinaTietjen@w134-87-131-075.wireless.uvic.ca>
2016-07-08 13:54:53 -0700

    worked on combining providences into Canada and states into USA and a plot was made

--- analyses/explore_survey_data2.R
+++ analyses/explore_survey_data2.R
@@ -4,7 +4,7 @@
 
 setwd("/Users/jpwrobinson/Documents/git_repos/gya-research")
 
-setwd("/Users/kristinatietjen/Documents/gya-research")
+setwd("/Users/kristinatietjen/Documents/git_hub/gya-research")
 
 
 
@@ -29,20 +29,43 @@ survey<-survey[survey$Status=="Complete",]
 
 colnames(survey)
 
-survey<-subset(survey, select=c("Location",  "gender", "field_research"))
+survey<-subset(survey, select=c("Location",  "gender", "field_research", "Country_work"))
+
+
+states<-c("California","New York", "Pennsylvania", "Nebraska", "Massachusetts", "Vermont","Texas",
+          "Michigan", "Maryland", "Florida", "Washington", "Oregon", "Nevada", "Minnesota", "Arizona",
+          "Wisconsin", "Virginia", "Utah", "Ohio", "North Carolina", "New Jersey", "New Hampshire", 
+          "Maine", "Louisiana", "Indiana", "Hawaii", "Alabama", "Tennessee", "Oklahoma", "New Mexico",
+          "Missouri", "Mississippi", "Iowa", "Delaware", "Colorado", "Illinois")
+
+prov<-c("Ontario", "Quebec", "British Columbia", "Alberta", "Nova Scotia", "New Brunswick", 
+        "Newfoundland and Labrador", "Manitoba", "Saskatchewan", "Prince Edward Island", "Yukon Territory", 
+        "Nunavut")
+
+survey$Country<-as.character(survey$Location)
+survey$Country<-ifelse(survey$Country%in%states, 'USA', survey$Country)
+survey$Country<-ifelse(survey$Country%in%prov, 'Canada', survey$Country)
+survey$Country<-as.factor(survey$Country)
 
 ################################
 #### Summary statistics ########
 ################################
 
-locations<-aggregate(gender ~ Location, survey, length)
+locations<-aggregate(gender ~ Country, survey, length)
 gender<-aggregate(Location ~ gender, survey, length)
 field<-aggregate(Location ~ field_research, survey, length)
+country_work<-aggregate(gender ~ Country_work, survey, length)
 
 
-head(locations)
 
-locations[order(locations$gender, locations$Location, decreasing=TRUE),]
 
+locations[order(locations$gender, locations$Country, decreasing=TRUE),]
+
+
+require(ggplot2)
+country_work<-transform(country_work, 
+          Country_work = reorder(Country_work, gender))
+ggplot(country_work, aes(Country_work, gender)) + geom_bar(stat='identity') + 
+  theme(axis.text.x=element_text(angle=90)) + labs(y="Number of responses") 
 
 
