0545bd4bcd4ce4264323c55bb1316042938bf291
Kristina Tietjen <KristinaTietjen@w134-87-132-089.wireless.uvic.ca>
2016-07-12 10:04:04 -0700

    took out % and made a few edits to graphs

--- analyses/explore_survey_data2.R
+++ analyses/explore_survey_data2.R
@@ -57,6 +57,7 @@ survey.table<-table(survey$Country,survey$gender)
 
 
 gender<-aggregate(Location ~ gender, survey, length)
+head(gender)
 
 country_work<-aggregate(gender ~ Country_work, survey, length)
 
@@ -101,7 +102,10 @@ locations<-aggregate(gender ~ Country, survey, length)
 ggplot(country_work, aes(x = reorder(Country_work, -gender), gender)) + geom_bar(stat='identity',position = position_dodge(width=0.5)) + 
   theme(axis.text.x=element_blank()) + labs(y="Number of responses",x="")  + 
   geom_text(aes(label=gender), vjust=-0.25) +
-  geom_text(aes(label=Country_work), angle=90, hjust=-0.5) + lims(y=c(0, 1300)) + theme(legend.title=element_text(size=12), legend.text=element_text(size=10), axis.text=element_text(size=14), axis.title=element_text(size=16))
+  geom_text(aes(label=Country_work), angle=90, hjust=-0.5) + lims(y=c(0, 1300)) + theme(legend.title=element_text(size=12), 
+  legend.text=element_text(size=10), axis.text=element_text(size=14), axis.title=element_text(size=16)) + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
 
 
 ##2a # of responses by field research 
@@ -109,8 +113,8 @@ field<-aggregate(Location ~ field_research, survey, length)
 
 ggplot(field, aes(x = reorder(field_research, -Location), Location, fill=field_research)) + geom_bar(stat='identity',position = position_dodge(width=0.5)) + 
   theme(axis.text.x=element_blank()) + labs(y="Number of responses",x="")  + scale_fill_discrete(name="Field of Research") + 
-  geom_text(aes(label=Location), vjust=-0.25) + theme(legend.position=c(0.8, 0.8)) + theme(legend.title=element_text(size=12), legend.text=element_text(size=10), axis.text=element_text(size=14), 
-  axis.title=element_text(size=14)) + guides(fill=guide_legend(reverse=TRUE))
+  geom_text(aes(label=Location), vjust=-0.25) + theme(legend.position=c(0.78, 0.8)) + theme(legend.title=element_text(size=12), legend.text=element_text(size=10), axis.text=element_text(size=14), 
+  axis.title=element_text(size=14)) + guides(fill=guide_legend(reverse=TRUE)) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
 
 
@@ -118,12 +122,14 @@ ggplot(field, aes(x = reorder(field_research, -Location), Location, fill=field_r
 canada<-subset(survey, Country=="Canada")
 
 field<-aggregate(Location ~ field_research, canada, length)
-head(field)
+
 
 ggplot(field, aes(x = reorder(field_research, -Location), Location, fill=field_research)) + geom_bar(stat='identity',position = position_dodge(width=0.5)) + 
   theme(axis.text.x=element_blank()) + labs(y="Number of responses",x="")  + scale_fill_discrete(name="Field of Research") +
-  geom_text(aes(label=Location), vjust=-0.25) + theme(legend.position=c(0.8, 0.8)) + theme(legend.title=element_text(size=12), 
-  legend.text=element_text(size=10), axis.text=element_text(size=14), axis.title=element_text(size=16))+ guides(fill=guide_legend(reverse=TRUE))
+  geom_text(aes(label=Location), vjust=-0.25) + theme(legend.position=c(0.78, 0.8)) + theme(legend.title=element_text(size=12), 
+  legend.text=element_text(size=10), axis.text=element_text(size=14), axis.title=element_text(size=16))+ 
+  guides(fill=guide_legend(reverse=TRUE)) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
 
 
 ##3a # of responses by participant group 
@@ -131,8 +137,9 @@ experience<-aggregate(Location ~ what_participant_group, survey, length)
 
 ggplot(experience, aes(x = reorder(what_participant_group, -Location), Location, fill=what_participant_group)) + geom_bar(stat='identity',position = position_dodge(width=0.5)) +
   labs(y="Number of responses", x="") +  theme(legend.position="bottom") + theme(axis.text.x=element_blank()) + 
-  geom_text(aes(label=Location),size=5, vjust=-0.25) + guides(fill=guide_legend(reverse=TRUE, nrow=6)) + scale_fill_discrete(name="Participant Group") + 
-  theme(legend.title=element_text(size=12), legend.text=element_text(size=10), axis.text=element_text(size=14), axis.title=element_text(size=14))
+  geom_text(aes(label=Location),size=4, vjust=-0.25) + guides(fill=guide_legend(title=NULL, reverse=TRUE, nrow=6)) + scale_fill_discrete(name="Participant Group") + 
+  theme(legend.title=element_text(size=12), legend.text=element_text(size=10), axis.text=element_text(size=14), 
+  axis.title=element_text(size=14))+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) 
  
 
 ##3b # of responses by participant group for canada
@@ -142,8 +149,10 @@ experience<-aggregate(Location ~ what_participant_group, canada, length)
 
 ggplot(experience, aes(x = reorder(what_participant_group, -Location), Location, fill=what_participant_group)) + geom_bar(stat='identity',position = position_dodge(width=0.5)) +
   labs(y="Number of responses", x="") +  theme(legend.position="bottom") + theme(axis.text.x=element_blank()) + 
-  geom_text(aes(label=Location),size=5, vjust=-0.25)+ guides(fill=guide_legend(reverse=TRUE, nrow=6)) + scale_fill_discrete(name="Participant Group") + 
-  theme(legend.title=element_text(size=12), legend.text=element_text(size=10), axis.text=element_text(size=14), axis.title=element_text(size=16))
+  geom_text(aes(label=Location),size=4, vjust=-0.25)+ guides(fill=guide_legend(title=NULL, reverse=TRUE, nrow=6)) + scale_fill_discrete(name="Participant Group") + 
+  theme(legend.title=element_text(size=12), legend.text=element_text(size=10), axis.text=element_text(size=14), axis.title=element_text(size=16))+ 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
 
 
 ## read in non-subsetted data again
@@ -153,9 +162,17 @@ survey$percent_Applied_Research_past<-as.character(survey$percent_Applied_Resear
 
 require(stringr)
 ## remove all % symbols 
-survey$percent_Applied_Research_past<-str_replace_all().........
+survey$percent_Applied_Research_past<-str_replace_all(survey$percent_Applied_Research_past, "[%]", "")
 
-##4a type of research 50% or greater of fundemental
+survey<-subset(survey, select=c("Location",  "gender", "percent_fundemental_research_current", "percent_Applied_Research_current", 
+                                "percent_Use_inspired_Research_current"))
+
+
+##4a type of research 50% or greater of fundemental ---?
+
+
+
+
 
 ##4b canada
 canada<-subset(survey, Country=="Canada")
@@ -163,6 +180,12 @@ canada<-subset(survey, Country=="Canada")
 
 
 ##5a box plot of percent of each type of research by field
+fundemental<-aggregate(Location ~ percent_fundemental_research_current, survey, length)
+applied<-aggregate(Location ~ percent_Applied_Research_current, survey, length)
+use<-aggregate(Location ~ percent_Use_inspired_Research_current, survey, length)
+
+ggplot()
+
 
 
 ##5b canada
