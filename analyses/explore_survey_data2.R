c2e0c7cf686795e1fd8faa8df0f56f84d6b121ea
Kristina Tietjen <KristinaTietjen@w134-87-132-089.wireless.uvic.ca>
2016-07-18 11:06:25 -0700

    edited and read in new data

--- analyses/explore_survey_data2.R
+++ analyses/explore_survey_data2.R
@@ -9,15 +9,54 @@ setwd("/Users/kristinatietjen/Documents/git_hub/gya-research")
 
 
 ## read data
-survey<-read.csv("data/July-7-2016-7pm-Toronto_simplified.csv", header=TRUE)
-#colnames(survey)<-as.character(unlist(survey[1,]))
-#survey<-survey[-1,]
+#survey.simp<-read.csv("data/July-7-2016-7pm-Toronto_simplified.csv", header=TRUE)
+survey<-read.csv("data/Jul 18 2016 1149am - Hamilton.csv", header=TRUE)
 
 
+suveycols<-c("Status","Location"	,"what_participant_group",
+  "percent_fundemental_research_current",	"percent_Use_inspired_Research_current"	,
+  "percent_Applied_Research_current",	"changed_10yrs",	"percent_Fundamental_Research_past"	,
+  "percent_Use_inspired_Research_past",	"percent_Applied_Research_past",	"Main_reason_change_interest_related",
+  "Main_reason_change_Career_related",	"Main_reason_change_Funding_related",	"Main_reason_change_Socially_related",
+  "Main_reason_change_Other",	"Main_reason_change_Other_text",	"view_change_of_type",
+  "partnership_outside",	"partnership_change_10yrs",	"partnership_outside_before",	
+  "reason_partnership_change_interest",	"reason_partnership_change_career",	"reason_partnership_change_socially",
+  "reason_partnership_change_funding",	"reason_partnership_change_other",	"reason_partnership_change_other_text",
+  "view_change_partnership",	"external_pi_grant_11_15_fundamental",	"external_pi_grant_11_15_use",
+  "external_pi_grant_11_15_applied","external_pi_grant_6_10_fundamental",	"external_pi_grant_6_10_use",
+  "external_pi_grant_6_10_applied",	"successful_grants_11_15_fundamental",	"successful_grants_11_15_use",
+  "successful_grants_11_15_applied",	"successful_grants_6_10_fundamental",	"successful_grants_6_10_use",
+  "successful_grants_6_10_applied",	"practical_applications_important_11_15",	"practical_applications_important_6_10",	
+  "include_nonacademia_partners_success_11_15", "include_nonacademia_partners_success_6_10",
+  "distribution_funding_11_15_internal",	"distriution_funding_11_15_government",	"distriution_funding_11_15_for_profit",	
+  "distriution_funding_11_15_nongov",	"distriution_funding_11_15_other",	"distriution_funding_11_15_other_text",
+  "distriution_funding_6_10_internal",	"distriution_funding_6_10_government",	"distriution_funding_6_10_for_profit",
+  "distriution_funding_6_10_nongov",	"distriution_funding_6_10_other",	"distriution_funding_6_10_other_text",
+  "success_change_10yrs_fundamental",	"success_change_10yrs_use",	"success_change_10yrs_applied",
+  "opinion_fundamental_important",	"high_priority_fundamental",	"high_priority_use_inspired",
+  "high_priority_applied",	"high_priority_no_change",	"high_priority_comments",	"available_funding_fundamental",
+  "available_funding_use_inspired",	"available_funding_applied",	"next_generation",	"next_generation_Comments",
+  "field_research",	"PhD_Year",	"Country_work",	"gender")
 ## remove some unnecessary columns
-#survey$Username<-NULL
-#survey$'Updated At'<-NULL
-#survey'Number of Saves'<-NULL
+survey$Username<-NULL
+survey$'Updated_At'<-NULL
+survey$'Number_of_Saves'<-NULL
+survey$'Internal_ID'<-NULL
+survey$'Language'<-NULL
+survey$'Created_At'<-NULL
+survey$'GET_Variables'<-NULL
+survey$'Referrer'<-NULL
+survey$'Weighted_Score'<-NULL
+survey$'Completion_Time'<-NULL
+survey$'IP_Address'<-NULL
+survey$'Invite_Code'<-NULL
+survey$'Invite_Email'<-NULL
+survey$'Invite_Name'<-NULL
+survey$'Collector'<-NULL
+survey$'final_comments'<-NULL
+##change column names
+
+colnames(survey)<-suveycols
 
 ## check number of complete vs. incomplete
 table(survey$Status)
@@ -29,7 +68,7 @@ survey<-survey[survey$Status=="Complete",]
 #require(stringr)
 #survey$date<-str_split_fixed(survey$'Completed At', " ", 1)
 
-colnames(survey)
+
 #############
 ####Part5####
 #############
