1d38cd93823f1ae05c041fa9de259a1bcff140f3
James Robinson <jpwrobinson@gmail.com>
2016-07-20 14:31:30 -0700

    started map figure script

---
+++ gya_map_figure.R
@@ -0,0 +1,50 @@
+
+
+## Creating map of responses
+library(rworldmap)
+library(ggplot2)
+library(RColorBrewer)
+library(maptools)
+
+
+
+#map.world <- map_data(map="world")
+
+#setwd("/Users/kristinatietjen/Documents/git_hub/gya-research")
+setwd("/Users/IMAC3/Documents/git-jpwrobinson/gya-research")
+
+survey<-read.csv(file="data/gya-without-incomplete.csv")
+country_work<-aggregate(gender ~ Country_work, survey, length)
+
+
+
+data(wrld_simpl)
+# this lets us use the contry name vs 3-letter ISO
+wrld_simpl@data$id <- wrld_simpl@data$NAME
+
+wrld <- fortify(wrld_simpl, region="id")
+wrld <- subset(wrld_simpl, id != "Antarctica") # we don't need Antarctica
+
+gg <- ggplot()
+
+# setup base map
+gg <- gg + geom_map(data=wrld, map=wrld, aes(map_id=id, x=long, y=lat), fill="white", color="#7f7f7f", size=0.25)
+
+# add our colored regions
+gg <- gg + geom_map(data=country_work, map=wrld, aes(map_id=Country_work, fill=gender),  color="white", size=0.25)
+gg
+
+
+
+
+# this gives us proper coords. mercator proj is default
+gg <- gg + coord_map()
+gg <- gg + labs(x="", y="")
+gg <- gg + theme(plot.background = element_rect(fill = "transparent", colour = NA),
+                 panel.border = element_blank(),
+                 panel.background = element_rect(fill = "transparent", colour = NA),
+                 panel.grid = element_blank(),
+                 axis.text = element_blank(),
+                 axis.ticks = element_blank(),
+                 legend.position = "right")
+gg
---
+++ open-git-setup.md
@@ -0,0 +1,34 @@
+
+## Open Science workflow project
+
+## Setup for new git users 
+
+
+
+
+Installation
+
+Ensure the following programs are installed on your personal computer.
+
+R Studio [link]
+Macdown [http://macdown.uranusjr.com/] OR windows equivalent?
+Tex [Mac: http://tug.org/mactex/] [Windows: http://miktex.org/2.9/setup]
+
+
+To get setup for Git, run the following code in your terminal:
+
+http://git-scm.com/download/mac
+http://git-scm.com/download/win
+
+
+To configure Git for your Github account, run the following code in your terminal:
+
+git config....
+
+
+
+Large File Storage.
+
+
+
+
