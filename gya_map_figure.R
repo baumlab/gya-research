835bee97a48c8fc65fd3b177efc6b5da365b51f6
Kristina Tietjen <kristinaltietjen@gmail.com>
2017-05-15 15:36:23 -0700

    created maps with the responses for survey

--- gya_map_figure.R
+++ gya_map_figure.R
@@ -5,6 +5,7 @@ library(rworldmap)
 library(ggplot2)
 library(RColorBrewer)
 library(maptools)
+library(mapproj)
 
 
 
@@ -19,22 +20,32 @@ nations<-aggregate(gender ~ nation, survey, length)
 
 
 data(wrld_simpl)
-# this lets us use the contry name vs 3-letter ISO
+# this lets us use the country name vs 3-letter ISO
 wrld_simpl@data$id <- wrld_simpl@data$NAME
 
 wrld <- fortify(wrld_simpl, region="id")
-wrld <- subset(wrld_simpl, id != "Antarctica") # we don't need Antarctica
+wrld <- subset(wrld, id != "Antarctica") # we don't need Antarctica
+#wrld <- wrld_simpl[wrld_simpl$id!="Antarctica",]
+#wrld <- fortify(wrld_simpl, region="id")
+
 
 gg <- ggplot()
 
 # setup base map
+#gg <- gg + geom_map(data=wrld, map=wrld, aes(map_id=id), fill="white", color="#7f7f7f", size=0.25)
+
 gg <- gg + geom_map(data=wrld, map=wrld, aes(map_id=id, x=long, y=lat), fill="white", color="#7f7f7f", size=0.25)
 
 # add our colored regions
-gg <- gg + geom_map(data=nations, map=wrld, aes(map_id=nations, fill=gender),  color="white", size=0.25)
+gg <- gg + geom_map(data=nations, map=wrld, aes(map_id=nation, fill=gender),  color="white", size=0.25)
+
+#gg<- gg +scale_fill_continuous(expand=c(0,0), limits=c(0,1350),breaks=c(0,50,100,150, 200,250, 500, 750, 1000, 1250))
+
+pdf(file="figures/map_1.pdf", width = 11, height= 7)
+
 gg
 
-
+dev.off()
 
 
 # this gives us proper coords. mercator proj is default
@@ -47,4 +58,72 @@ gg <- gg + theme(plot.background = element_rect(fill = "transparent", colour = N
                  axis.text = element_blank(),
                  axis.ticks = element_blank(),
                  legend.position = "right")
+
+pdf(file="figures/map_proper.coords_1.pdf", width=11, height= 7)
 gg
+
+dev.off()
+
+ ###### ##### ##### ##### ##### ##### #####
+###### now do it all with out Canada ########
+ ###### ##### ##### ##### ##### ##### #####
+
+#load data
+survey<-read.csv(file="data/gya-without-incomplete.csv")
+
+#remove Canada
+n.gbl.wCA <- survey[survey$nation!="Canada",]
+nations.wCA<-aggregate(gender ~ nation, n.gbl.wCA, length)
+
+#now seperate out Canada so it can be a different color
+canada <- survey[survey$nation=="Canada",]
+ca<-aggregate(gender ~ nation, canada, length)
+
+
+data(wrld_simpl)
+# this lets us use the country name vs 3-letter ISO
+wrld_simpl@data$id <- wrld_simpl@data$NAME
+
+wrld <- fortify(wrld_simpl, region="id")
+wrld <- subset(wrld, id != "Antarctica") # we don't need Antarctica
+#wrld <- wrld_simpl[wrld_simpl$id!="Antarctica",]
+#wrld <- fortify(wrld_simpl, region="id")
+
+
+gg <- ggplot()
+
+# setup base map
+#gg <- gg + geom_map(data=wrld, map=wrld, aes(map_id=id), fill="white", color="#7f7f7f", size=0.25)
+
+gg <- gg + geom_map(data=wrld, map=wrld, aes(map_id=id, x=long, y=lat), fill="white", color="#7f7f7f", size=0.25)
+
+# add our colored regions (not canada)
+gg <- gg + geom_map(data=nations.wCA, map=wrld, aes(map_id=nation, fill=gender),  color="white", size=0.25)
+# add our colored regions (canada)
+#gg <- gg + geom_map(data=ca, map=wrld, aes(map_id=nation, fill=gender), fill="tomato2",  color="white", size=0.25)+geom_text(aes(label=ca$gender, x=-110, y=58), size=5)     # saved as red
+#gg <- gg + geom_map(data=ca, map=wrld, aes(map_id=nation, fill=gender), fill="mediumseagreen",  color="white", size=0.25)+geom_text(aes(label=ca$gender, x=-110, y=58), size=5)  # saved as green2
+#gg <- gg + geom_map(data=ca, map=wrld, aes(map_id=nation, fill=gender), fill="springgreen3",  color="white", size=0.25)+geom_text(aes(label=ca$gender, x=-110, y=58), size=5)    # saved as green
+#gg <- gg + geom_map(data=ca, map=wrld, aes(map_id=nation, fill=gender), fill="slategray2",  color="white", size=0.25)+geom_text(aes(label=ca$gender, x=-110, y=58), size=5)     #saved as lightblue
+#gg <- gg + geom_map(data=ca, map=wrld, aes(map_id=nation, fill=gender), fill="grey",  color="white", size=0.25)+geom_text(aes(label=ca$gender, x=-110, y=58), size=5)      # saved as grey
+
+
+#pdf(file="figures/map_w.out_CA_1.pdf", width = 11, height= 7)
+gg
+#dev.off()
+
+
+# this gives us proper coords. mercator proj is default
+gg <- gg + coord_map()
+gg <- gg + labs(x="", y="")
+gg <- gg + theme(plot.background = element_rect(fill = "transparent", colour = NA),
+                 panel.border = element_blank(),
+                 panel.background = element_rect(fill = "transparent", colour = NA),
+                 panel.grid = element_blank(),
+                 axis.text = element_blank(),
+                 axis.ticks = element_blank(),
+                 legend.position = "right")
+
+pdf(file="figures/map_w.out_CA_proper.coords_1_grey.pdf", width=11, height= 7)
+gg
+
+dev.off()
